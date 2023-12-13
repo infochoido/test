7e4c57f9316daefa526e55c958f6d39e5a3594ce
infochoido <130339947+infochoido@users.noreply.github.com>
2023-12-13 13:34:44 +0900

    g

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,128 @@
-export default function MyPage(){
-    return(
-        <div>
-        마이페이지
-        </div>
-    )
+import React, { useState, useEffect } from "react";
+import { updateDoc, doc, collection, getDocs, query, where } from 'firebase/firestore';
+import { auth, db, getUser } from '../firebase';
+
+export default function MyPage() {
+  const [user, setUser] = useState(null);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    nickname: "",
+    email: "",
+    profilePicture: "", // 프로필 사진 URL
+  });
+
+  useEffect(() => {
+    const unsubscribe = getUser((userData) => {
+      console.log("User data from getUser:", userData);
+      setUser(userData);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      const userEmail = user.email;
+
+      getUserInfoByEmail(userEmail).then((userInfo) => {
+        if (userInfo) {
+          setUserInfo(userInfo);
+        } else {
+          setUserInfo({
+            name: "",
+            nickname: "",
+            email: "",
+            profilePicture: "",
+          });
+        }
+      });
+    } else {
+      setUserInfo({
+        name: "",
+        nickname: "",
+        email: "",
+        profilePicture: "",
+      });
+    }
+  }, [user]);
+
+  const getUserInfoByEmail = async (userEmail) => {
+    try {
+      const q = query(collection(db, 'users'), where("email", "==", userEmail));
+      const querySnapshot = await getDocs(q);
+  
+      if (!querySnapshot.empty) {
+        const userData = querySnapshot.docs[0].data();
+        return {
+          name: userData.name,
+          nickname: userData.nickname,
+          email: userEmail, // Include the email in userInfo
+        };
+      } else {
+        console.error("해당 이메일을 가진 사용자가 없습니다.");
+        return null;
+      }
+    } catch (error) {
+      console.error("사용자 정보 가져오기 중 오류가 발생했습니다:", error.message);
+      return null;
+    }
+  };
+  
+
+  const handleUpdateProfile = async (updatedData) => {
+    try {
+      const currentUser = auth.currentUser;
+
+      if (currentUser) {
+        const userEmail = currentUser.email;
+
+        const q = query(collection(db, 'users'), where("email", "==", userEmail));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+          const userData = querySnapshot.docs[0].data();
+
+          const userId = querySnapshot.docs[0].id;
+          const userDocRef = doc(collection(db, 'users'), userId);
+
+          await updateDoc(userDocRef, {
+            name: updatedData.name || userData.name,
+            nickname: updatedData.nickname || userData.nickname,
+          });
+
+          console.log("사용자 정보가 성공적으로 업데이트되었습니다.");
+          setUserInfo((prevUserInfo) => ({ ...prevUserInfo, ...updatedData }));
+        } else {
+          console.error("해당 이메일을 가진 사용자가 없습니다.");
+        }
+      } else {
+        console.error("사용자 정보를 업데이트할 수 없습니다. 사용자가 로그인되어 있지 않습니다.");
+      }
+    } catch (error) {
+      console.error("사용자 정보 업데이트 중 오류가 발생했습니다:", error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>마이페이지</h1>
+      <div>
+        <p>이름: {userInfo.name}</p>
+        <p>닉네임: {userInfo.nickname}</p>
+        <p>이메일: {userInfo.email}</p>
+        <img
+          src={userInfo.profilePicture}
+          alt="프로필 사진"
+          style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+          className="border-2 border-black"
+        />
+      </div>
+      <div className="flex flex-col">
+        <button onClick={() => handleUpdateProfile({ name: "새로운 이름" })}>이름 수정</button>
+        <button onClick={() => handleUpdateProfile({ nickname: "새로운 닉네임" })}>닉네임 수정</button>
+        {/* 프로필 사진 업데이트 기능은 추가적인 구현이 필요합니다. */}
+        <input type="file" onChange={(e) => console.log("프로필 사진 업데이트 기능 구현 필요")} />
+      </div>
+    </div>
+  );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,124 +11,210 @@ import Divider from '@mui/material/Divider';
 import InputLabel from "@mui/material/InputLabel";
 import Paper from '@mui/material/Paper';
 import { signupEmail } from "../firebase";
+import { db } from "../firebase";
+import { addDoc, collection, query, where, getDocs } from 'firebase/firestore';
 
-export const Signup= () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirm, setPasswordConfirm] = useState('');
+export const Signup = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [name, setName] = useState(''); // 이름을 위한 state 추가
+  const [nickname, setNickname] = useState(''); // 닉네임을 위한 state 추가
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
 
-    const handleSignup = async () => {
-        if (password !== passwordConfirm) {
-          console.log("Passwords do not match");
-          return;
-        }
+  const handleCheckboxChange = (event) => {
+    setCheckboxChecked(event.target.checked);
+  };
 
-        try {
-            // Call the signupEmail function from your Firebase setup
-            const userCredential = await signupEmail(email, password);
-            console.log("User signed up:", userCredential.user);
-            alert("회원가입 성공!!")
-            navigate("/");
-            // You can perform additional actions after successful signup if needed
-          } catch (error) {
-            console.error("Error signing up:", error.message);
+  const handleSignup = async () => {
+    if (!email || !password || !passwordConfirm || !checkboxChecked || !name || !nickname) {
+      setAlertMessage("모든 필드를 입력하고 약관에 동의해주세요.");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 2300); // 2초 후에 숨김
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      setAlertMessage("비밀번호와 확인이 일치하지 않습니다.");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 2300); // 2초 후에 숨김
+      return;
+    }
+
+    try {
+
+        const nicknameQuery = query(collection(db, 'users'), where('nickname', '==', nickname));
+    const nicknameSnapshot = await getDocs(nicknameQuery);
+
+    if (!nicknameSnapshot.empty) {
+      setAlertMessage("이미 사용 중인 닉네임입니다.");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 2300); // 2초 후에 숨김
+      return;
+    }
+
+      // Firebase Firestore에 추가로 유저 정보 저장
+      const userCredential = await signupEmail(email, password);
+      const user = userCredential.user;
+     
+      const userDocRef = await addDoc(collection(db, 'users'), {
+        name: name,
+        nickname : nickname,
+        email : email,
+      });
+
+      console.log("User signed up:", user);
+      alert("회원가입 성공!!");
+      navigate("/");
+      // 추가로 필요한 작업이 있다면 여기에서 수행
+    } catch (error) {
+        if (error.code === 'auth/email-already-in-use') {
+            setAlertMessage("이미 사용 중인 이메일입니다.");
+            setShowAlert(true);
+            setTimeout(() => {
+              setShowAlert(false);
+            }, 2300); // 2초 후에 숨김
+          } else {
+            console.error("회원가입 오류:", error.message);
           }
-        };
+    }
+  };
 
-        return (
-            <div className="flex items-center justify-center h-[80vh] align-middle">
-              <Paper sx={{
-                width: 350,
-                py: 3,
-                px: 2
-              }} className="flex flex-col items-center">
-                <Typography variant={"h5"} component={"h1"}>회원가입</Typography>
-                <Box
-                  component={"form"}
-                  autoComplete="off"
-                  className="flex flex-col"
-                >
-                  <Stack spacing={1}>
-                    <div className="flex flex-col">
-                      <InputLabel>
-                        <Typography variant={"overline"} component={"h2"}>
-                          이메일
-                        </Typography>
-                      </InputLabel>
-                      <TextField
-                        size={"small"}
-                        fullWidth
-                        placeholder="example@naver.com"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <InputLabel>
-                        <Typography variant={"overline"} component={"h2"}>
-                          비밀번호
-                        </Typography>
-                      </InputLabel>
-                      <TextField
-                        id="password"
-                        type={"password"}
-                        size={"small"}
-                        fullWidth
-                        placeholder="********"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <InputLabel>
-                        <Typography variant={"overline"} component={"h2"}>
-                          비밀번호 확인
-                        </Typography>
-                      </InputLabel>
-                      <TextField
-                        id="passwordConfirm"
-                        type={"password"}
-                        size={"small"}
-                        fullWidth
-                        placeholder="********"
-                        value={passwordConfirm}
-                        onChange={(e) => setPasswordConfirm(e.target.value)}
-                      />
-                    </div>
-                  </Stack>
-                  <Button variant="contained" sx={{ mt: 4 }} onClick={handleSignup}>
-                    가입하기
-                  </Button>
-        <div className="flex flex-col items-center w-full">
-          <Typography variant={"caption"} sx={{
-            mt: 2,
-          }}>
-            <p>가입 시, 최도현 커뮤니티가 제공하는 서비스를 모두 이용하실 수 있습니다.</p>
-            <p>서비스 이용약관, 개인정보처리방침에 동의합니다.</p>
+  return (
+    <div className="flex items-center justify-center h-[100vh] align-middle mt-10 mb-24">
+      <Paper sx={{
+        width: 280,
+        py: 1,
+        px: 2
+      }} className="flex flex-col items-center">
+        <Typography variant={"h8"} component={"h2"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>회원가입</Typography>
+        <Box
+          component={"form"}
+          autoComplete="off"
+          className="flex flex-col"
+        >
+          <Stack spacing={1}>
+            <div className="flex flex-col">
+              <InputLabel>
+                <Typography variant={"overline"} component={"h3"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+                  닉네임
+                </Typography>
+              </InputLabel>
+              <TextField
+                size={"small"}
+                fullWidth
+                placeholder="이훈식"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <InputLabel>
+                <Typography variant={"overline"} component={"h3"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+                  이름
+                </Typography>
+              </InputLabel>
+              <TextField
+                size={"small"}
+                fullWidth
+                placeholder="최도현"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <InputLabel>
+                <Typography variant={"overline"} component={"h3"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+                  이메일
+                </Typography>
+              </InputLabel>
+              <TextField
+                size={"small"}
+                fullWidth
+                placeholder="example@naver.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <InputLabel>
+                <Typography variant={"overline"} component={"h3"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+                  비밀번호
+                </Typography>
+              </InputLabel>
+              <TextField
+                id="password"
+                type={"password"}
+                size={"small"}
+                fullWidth
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <InputLabel>
+                <Typography variant={"overline"} component={"h3"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+                  비밀번호 확인
+                </Typography>
+              </InputLabel>
+              <TextField
+                id="passwordConfirm"
+                type={"password"}
+                size={"small"}
+                fullWidth
+                placeholder="********"
+                value={passwordConfirm}
+                onChange={(e) => setPasswordConfirm(e.target.value)}
+              />
+            </div>
+          </Stack>
+          <Button variant="contained" sx={{ mt: 4 }} onClick={handleSignup} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+            가입하기
+          </Button>
+          <div className="flex flex-col items-center w-full">
+            <Typography variant="caption" sx={{ mt: 2, fontSize: '12px' }}>
+              <p style={{ fontFamily: 'TheJamsil5Bold, sans-serif', fontWeight: '300', fontSize: '11px' }}>가입 시, 최도현 커뮤니티가 제공하는 서비스를 모두 이용하실 수 있습니다.</p>
+              <p style={{ fontFamily: 'TheJamsil5Bold, sans-serif', fontWeight: '300', fontSize: '11px' }}>서비스 이용약관, 개인정보처리방침에 동의합니다.</p>
+            </Typography>
+            <FormControlLabel control={<Checkbox checked={checkboxChecked} onChange={handleCheckboxChange} size={"small"} />}
+              label={<p className="relative right-2" style={{ fontFamily: 'TheJamsil5Bold, sans-serif', fontSize: '11px' }}>가입한 이메일로 유용한 소식을 받아볼래요.</p>}
+              componentsProps={{
+                typography: { variant: 'caption' }
+              }}
+            />
+          </div>
+        </Box>
+        <Divider sx={{ width: "100%", mt: 3 }}>
+          <Typography variant={"caption"} style={{ fontFamily: 'TheJamsil5Bold, sans-serif' }}>
+            간편 회원가입
           </Typography>
-          <FormControlLabel control={<Checkbox size={"small"} />}
-            label={<p className="relative right-2">가입한 이메일로 유용한 소식을 받아볼래요.</p>}
-            componentsProps={{
-              typography: {variant: "caption"}
-            }}
-          />
-        </div>
-      </Box>
-      <Divider sx={{width: "100%", mt: 3}}>
-        <Typography variant={"caption"}>
-          간편 회원가입
-        </Typography>
-      </Divider>
-      <Stack direction={"row"} spacing={2} sx={{mt: 2}}>
-        <button onClick={() => console.log("카카오 로그인시도")}>
-          <img alt="kakao" src={process.env.PUBLIC_URL + "/kakao-logo.svg"} className="w-[40px] h-[40px]"/>
-        </button>
-        <button onClick={() => console.log("네이버 로그인시도")}>
-          <img alt="naver" src={process.env.PUBLIC_URL + "/naver-logo.svg"} className="w-[40px] h-[40px]"/>
-        </button>
-      </Stack>
-    </Paper>
+        </Divider>
+        <Stack direction={"row"} spacing={2} sx={{ mt: 2 }}>
+          <button onClick={() => console.log("카카오 로그인시도")}>
+            <img alt="kakao" src={process.env.PUBLIC_URL + "/kakao-logo.svg"} className="w-[40px] h-[40px]" />
+          </button>
+          <button onClick={() => console.log("네이버 로그인시도")}>
+            <img alt="naver" src={process.env.PUBLIC_URL + "/naver-logo.svg"} className="w-[40px] h-[40px]" />
+          </button>
+        </Stack>
+        {showAlert && (
+          <div className="absolute top-0 mx-auto mt-4">
+            <Paper elevation={3} sx={{ p: 1, backgroundColor: "red", color: "white" }}>
+              {alertMessage}
+            </Paper>
+          </div>
+        )}
+      </Paper>
     </div>
   );
 }
